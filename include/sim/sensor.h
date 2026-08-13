8caac694f2d7afd38a35bfbecd7fbef3b92b3cc9
roozyfx <roozyfx@gmail.com>
2026-08-13 12:18:54 +0200

    21: Fixed mutex locking issues in Sensor class to prevent deadlocks and ensure thread safety

--- include/sim/sensor.h
+++ include/sim/sensor.h
@@ -1,8 +1,8 @@
 /* ***************************************************************************
  * A Sensor class, simulating a sensor, i.e. the concrete observer in the
- * simulation. It observes the subject, and adds uniform random noise in the range
- * specified in the configuration file, to simulate the measurement noise.
- * Each sensor also adds its ID and the measurement timestamp (in ms).
+ * simulation. It observes the subject, and adds uniform random noise in the
+ * range specified in the configuration file, to simulate the measurement noise.
+ * Each sensor also adds its ID and the measurement timestamp, in ms.
  * All packed as Position structure defined in the protobuffer file.
  * ***************************************************************************
  */
@@ -14,18 +14,22 @@
 
 #include "ifc_observer.h"
 #include "position.pb.h"
-#include "utils/config.h"
 #include "utils/rng.h"
 #include "utils/vectormath.h"
 
 namespace fx {
+using lock_guard = std::lock_guard<std::mutex>;
+
 template <std::floating_point T = float>
 class Sensor : public IObserver<Point<T>, T> {
  public:
   Sensor() : measurement_{}, rng_{}, mutex_{} {}
-  ~Sensor() = default;
+  ~Sensor() override = default;
 
-  inline void SetID(const uint64_t& id) { measurement_.set_id(id); }
+  inline void SetID(const uint64_t& id) {
+    lock_guard l(mutex_);
+    measurement_.set_id(id);
+  }
 
   /*
    * Measures, i.e. simulates measurement, of the current location of the
@@ -35,22 +39,22 @@ class Sensor : public IObserver<Point<T>, T> {
   void Measure(const Point<T>& p) final;
 
   inline void ReadMeasurement(Position& probe) const {
-    std::lock_guard l(mutex_);
+    lock_guard l(mutex_);
     probe = measurement_;
   }
 
   void PrintData() const;
 
  private:
+  Vector<T> GenerateNoise();
   /* Add measurement noise*/
-  void AddMeasurementNoise();
+  void AddMeasurementNoise(const Vector<T>& noise);
 
-  inline void SetPosition_M(const T& posx, const T& posy, const T& posz) {
+  inline void SetPosition_M(const T& pos_x, const T& pos_y, const T& pos_z) {
     // For thread-safety
-    std::lock_guard l(mutex_);
-    measurement_.mutable_position_m()->set_x(posx);
-    measurement_.mutable_position_m()->set_y(posy);
-    measurement_.mutable_position_m()->set_z(posz);
+    measurement_.mutable_position_m()->set_x(pos_x);
+    measurement_.mutable_position_m()->set_y(pos_y);
+    measurement_.mutable_position_m()->set_z(pos_z);
   }
 
   inline uint64_t CreateTimestamp() const {
--- src/sim/sensor.cpp
+++ src/sim/sensor.cpp
@@ -1,45 +1,57 @@
-#include <algorithm>
-#include <print>
-
 #include "sim/sensor.h"
 
-template <std::floating_point T>
-void fx::Sensor<T>::AddMeasurementNoise() {
-  T measurement_noise = T(Config<T>::GetValues().at("measurement_noise"));
-  T posx{}, posy{}, posz{};
-  {
-    std::lock_guard l(mutex_);
-    // Add (white) noise according to the measurement_noise in configuration
-    T noise_x{};
-    rng_.GenerateUniformRandom(noise_x, -measurement_noise, measurement_noise);
-    T noise_y{};
-    rng_.GenerateUniformRandom(noise_y, -measurement_noise, measurement_noise);
-    T noise_z{};
-    rng_.GenerateUniformRandom(noise_z, -measurement_noise, measurement_noise);
-    posx = std::clamp(measurement_.position_m().x() + noise_x, T(Config<T>::GetValues().at("x_min")),
-                      T(Config<T>::GetValues().at("x_max")));
-    posy = std::clamp(measurement_.position_m().y() + noise_y, T(Config<T>::GetValues().at("y_min")),
-                      T(Config<T>::GetValues().at("y_max")));
-    posz = std::clamp(measurement_.position_m().z() + noise_z, T(Config<T>::GetValues().at("z_min")),
-                      T(Config<T>::GetValues().at("z_max")));
-  }
+#include <algorithm>
+#include <concepts>
+#include <print>
 
-  SetPosition_M(posx, posy, posz);
+#include "utils/config.h"
+#include "utils/vectormath.h"
+
+template <std::floating_point T>
+fx::Vector<T> fx::Sensor<T>::GenerateNoise() {
+  T measurement_noise{
+      static_cast<T>(Config<T>::GetValues().at("measurement_noise"))};
+  // Add (white) noise according to the measurement_noise in configuration
+  T noise_x{};
+  rng_.GenerateUniformRandom(noise_x, -measurement_noise, measurement_noise);
+  T noise_y{};
+  rng_.GenerateUniformRandom(noise_y, -measurement_noise, measurement_noise);
+  T noise_z{};
+  rng_.GenerateUniformRandom(noise_z, -measurement_noise, measurement_noise);
+
+  return fx::Vector<T>(noise_x, noise_y, noise_z);
+}
+
+template <std::floating_point T>
+void fx::Sensor<T>::AddMeasurementNoise(const Vector<T>& noise) {
+  T pos_x{std::clamp(static_cast<T>(measurement_.position_m().x()) + noise.x,
+                     T(Config<T>::GetValues().at("x_min")),
+                     T(Config<T>::GetValues().at("x_max")))};
+  T pos_y{std::clamp(static_cast<T>(measurement_.position_m().y()) + noise.y,
+                     T(Config<T>::GetValues().at("y_min")),
+                     T(Config<T>::GetValues().at("y_max")))};
+  T pos_z{std::clamp(static_cast<T>(measurement_.position_m().z()) + noise.z,
+                     T(Config<T>::GetValues().at("z_min")),
+                     T(Config<T>::GetValues().at("z_max")))};
+
+  SetPosition_M(pos_x, pos_y, pos_z);
 }
 
 template <std::floating_point T>
 void fx::Sensor<T>::Measure(const Point<T>& p) {
+  auto noise{GenerateNoise()};
+  lock_guard l(mutex_);
   measurement_.set_timestamp_ms(CreateTimestamp());
 
   // Setting the position and then adding the noise, which in turn sets the
   // position again may seems redundant, but I find it more realistic
   SetPosition_M(p.x, p.y, p.z);
-  AddMeasurementNoise();
+  AddMeasurementNoise(noise);
 }
 
 template <std::floating_point T>
 void fx::Sensor<T>::PrintData() const {
-  std::lock_guard l(mutex_);
+  lock_guard l(mutex_);
   std::println("Sensor_{}:\t timestamp: {}, ({}, {}, {})",
                measurement_.id(), measurement_.timestamp_ms(),
                measurement_.position_m().x(), measurement_.position_m().y(),
